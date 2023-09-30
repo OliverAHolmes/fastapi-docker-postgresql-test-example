b79e3f606f9a5bed9dbd2f6d1cc0bb332a21ad5f
Oliver Holmes <oliver.albert.holmes@gmail.com>
2023-09-30 14:54:59 +0930

    Update config

--- config.py
+++ config.py
@@ -8,16 +8,6 @@ class Settings(BaseSettings):
     """A class used to manage application settings through environment variables."""
 
     ENV: str = "development"
-    db_path: str
-
-    @property
-    def db_path(self) -> str:
-        """Property method to decide which database path to use based on the environment.
-
-        Returns:
-            str: Path to the database file.
-        """
-        return "test.db" if self.ENV == "testing" else "database.db"
 
 
 # pylint: disable=too-few-public-methods
